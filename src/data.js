bad4c5cd1e2ebc8e6d36357acd2cf04430e0b2e2
anukkrit149 <anukkrit.official@gmail.com>
2024-07-04 16:14:38 +0200

    [init]

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export const data = {
+    "title": "Realms DAO Platform",
+    "image": "https://avatars.githubusercontent.com/u/89085176?s=200&v=4",
+    "description": "Vote on DAO governance proposals #1234.",
+    "label": "Vote",
+    "labelColor": "green",
+    "links": {
+        "actions": [
+            {
+                "label": "Vote Yes",
+                "color": "green",
+                "data": []
+            },
+            {
+                "label": "Vote No",
+                "color": "green",
+                "data": []
+            },
+            {
+                "label": "Abstain from Vote",
+                "color": "green",
+                "data": []
+            }
+        ]
+    }
+}
