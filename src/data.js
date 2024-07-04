96ddb5c8ac7a7154e85d41f4dc94c8b7a47e6591
Gaurav Gupta <gauravgupta1533@gmail.com>
2024-07-05 00:28:03 +1000

    UI fixes

--- src/data.js
+++ src/data.js
@@ -1,26 +1,25 @@
 export const data = {
-    "title": "Realms DAO Platform",
-    "image": "https://avatars.githubusercontent.com/u/89085176?s=200&v=4",
-    "description": "Vote on DAO governance proposals #1234.",
-    "label": "Vote",
-    "labelColor": "green",
-    "links": {
-        "actions": [
-            {
-                "label": "Vote Yes",
-                "color": "green",
-                "data": []
-            },
-            {
-                "label": "Vote No",
-                "color": "green",
-                "data": []
-            },
-            {
-                "label": "Abstain from Vote",
-                "color": "green",
-                "data": []
-            }
-        ]
-    }
-}
+	title: "Realms DAO Platform",
+	image: "https://avatars.githubusercontent.com/u/89085176?s=200&v=4",
+	description: "Vote on DAO governance proposals #1234.",
+	label: "Vote",
+	links: {
+		actions: [
+			{
+				label: "Vote Yes",
+				color: "green",
+				data: [],
+			},
+			{
+				label: "Vote No",
+				color: "red",
+				data: [],
+			},
+			{
+				label: "Abstain from Vote",
+				color: "grey",
+				data: [],
+			},
+		],
+	},
+};
